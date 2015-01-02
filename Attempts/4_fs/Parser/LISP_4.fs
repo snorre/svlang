d207621fa25c3103addf1dabfd5a3c1ca9003ca1
Snorre Visnes <snorre@entotre.org>
2015-01-03 00:02:45 +0100

    ..

--- Attempts/4_fs/Parser/LISP_4.fs
+++ Attempts/4_fs/Parser/LISP_4.fs
@@ -6,6 +6,7 @@ type simpleValue =
     | Bool of bool
     | Number of int
     | String of string
+    | StringList of string list
 
 type expr = // things that can be evaluated to simpleValue
     | Codeblock of expr list    
@@ -14,20 +15,68 @@ type expr = // things that can be evaluated to simpleValue
     | Reference of string
     | Assignment of string * expr // returns the assigned simpleValue
 
-
 (* Parser *)
-
-type Context =
-    struct
-        val variables : Map<string, simpleValue>
-    end
 
 let EvalValue (v:simpleValue) : obj =
     match v with
     | Bool b -> b :> obj
     | Number n -> n :> obj
     | String s -> s :> obj
+    | StringList sl -> sl :> obj
 
+type Context (variablesIn : Map<string, simpleValue>, functionsIn : Map<string, string list * expr list>) =
+    let mutable variables = variablesIn
+    let mutable functions = functionsIn
+
+    member this.Variables with get() = variables
+
+    member this.Functions with get() = functions
+
+    member this.findFunction (fn:string) : string list * expr list =
+        functions.[fn]
+
+    member this.addVariable (p:string * simpleValue) =
+        variables <- variables.Add p
+
+    member this.addFunction (fName:string) (fParams:string list) (fExprList:expr list) =
+        functions <- functions.Add (fName, (fParams, fExprList))
+
+    member this.copyAndAddParameter (pList:List<string * simpleValue>) : Context =
+        let newC = Context(variables, functions)
+        pList |> List.iter (fun kv -> newC.addVariable kv)
+        newC
+    
+    member this.copyAndAddFunction (fName:string) (fParams:string list) (fExprList:expr list) : Context =
+        let newC = Context(variables, functions)
+        newC.addFunction fName fParams fExprList
+        newC
+
+// TODO: Return true if function was overwritten?
+let builtin_defn (ctx:Context) (expressions:expr list) : simpleValue =
+    let fname = expressions.[0]
+    let paramNames = expressions.[1]
+    let exprList = expressions.[2]
+    match (fname, paramNames, exprList) with
+    | (Value fn, Value pnl, Codeblock cb) -> 
+        (
+            match fn, pnl with
+            | (String fnS, StringList pnlS) ->
+                ctx.addFunction fnS pnlS cb
+                Bool(false)
+            | _ -> raise (System.ArgumentException("Invalid arguments to defn"))
+        )
+    | (_,_,_) -> 
+        raise (System.ArgumentException("Invalid arguments to defn"))
+
+let builtin_plus (values:simpleValue list) : simpleValue =
+    let ints = values |> List.map (fun pv -> (EvalValue pv) :?> int)
+    Number(ints |> List.sum)
+
+let builtin_prt (values:simpleValue list) : simpleValue =
+    let strings = values |> List.map (fun v -> (EvalValue v))
+    let output = System.String.Join(" ", strings)
+    printfn "%s" output
+    String(output)
 
 let rec EvalExpr (e:expr) (ctx:Context) : simpleValue =
     match e with
@@ -37,14 +86,50 @@ let rec EvalExpr (e:expr) (ctx:Context) : simpleValue =
             |> List.rev
             |> List.head // Returns the last value
 
-    | Call (c, pel) -> 
-        // check builtins and call those
-        // check for existance
-        // call
-        String("implement me!")
+    | Call (fn, pel) -> 
+        match fn with
+        | "defn" -> builtin_defn ctx pel
+        | "+" -> builtin_plus (pel |> List.map (fun e -> EvalExpr e ctx))
+        | "prt" -> builtin_prt (pel |> List.map (fun e -> EvalExpr e ctx))
+        | _ -> 
+            let func_and_params = ctx.findFunction fn
+            let pnl = fst func_and_params |> List.toArray
+            let exprList = snd func_and_params
+            let pvel = pel |> List.map (fun p -> EvalExpr p ctx) |> List.toArray
+
+            EvalExpr (Codeblock(exprList)) (ctx.copyAndAddParameter (Array.zip pnl pvel |> List.ofArray))
 
     | Value v -> v
 
-    | Reference r -> String("implement me!")
+    | Reference r -> ctx.Variables.[r]
 
     | Assignment (n, e) -> String("implement me!")
+
+let Run code = EvalExpr code (Context(Map.empty<string, simpleValue>, Map.empty<string, string list * expr list>))
+
+
+let code_defn = 
+    Call(
+        "defn", 
+        [
+            Value(String("leggSammen")); 
+            Value(StringList(["a"; "b"])); 
+            Codeblock(
+                [ 
+                    Call("prt", [Value(String("Hei!")); Value(Bool(true)); Value(Number(123)); Value(StringList(["a"; "b"]))]);
+                    Call("prt", [Value(String("Legger sammen")); Reference("a"); Value(String("og")); Reference("b")]);
+                    Call("+", [Reference("a"); Reference("b") ])
+                ]
+            )
+        ]
+    )
+
+let code_call = 
+    Call(
+        "leggSammen",
+        [
+            Value(Number(2));
+            Value(Number(3))
+        ]
+    )
+let code = Codeblock([ code_defn; code_call ])
