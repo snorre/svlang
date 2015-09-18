ab7fd5d242b4be9584b02a2d2d5cae47040904ff
Snorre Visnes <snorre.visnes@gmail.com>
2015-09-19 01:56:38 +0200

    Treats warnings as errors

--- vm/Parser/Grammar/GenerateParser.bat
+++ vm/Parser/Grammar/GenerateParser.bat
@@ -1,3 +1,3 @@
 @echo off
 set self=%~dp0%~1
-java -jar %self%\antlr-4.5.1-complete.jar -o %self%\..\GeneratedParser -package SVLang.Parser.GeneratedParser -Dlanguage=CSharp %self%\SVLang.g4
+java -jar %self%\antlr-4.5.1-complete.jar -o %self%\..\GeneratedParser -package SVLang.Parser.GeneratedParser -Dlanguage=CSharp -Werror %self%\SVLang.g4
