3bb47c92c4e71fce15c1e232e573289122fc915a
Snorre Visnes <snorre.visnes@gmail.com>
2015-09-20 14:38:04 +0200

    Logs time spend executing files.

--- vm/Runner/FileRunner.cs
+++ vm/Runner/FileRunner.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using SVLang.Basics.AST;
 using SVLang.Core;
@@ -9,6 +10,7 @@ namespace SVLang.Runner
     public class FileRunner
     {
         private readonly FileInfo _file;
+        private string _timelog;
 
         public FileRunner(FileInfo file)
         {
@@ -22,11 +24,30 @@ namespace SVLang.Runner
 
         public Expr Run()
         {
+            var sw = new Stopwatch();
+            sw.Start();
+
             string code = File.ReadAllText(_file.FullName);
             code = "{" + Environment.NewLine + code + Environment.NewLine + "}";
+            Log("Read svl code", sw);
+
             var ast = new StringParser(code).Run();
+            Log("Parsed code", sw);
+
             var retVal = new Execution(ast).Run();
+            Log("Executed code", sw);
+
+            Console.WriteLine("Time usage: ");
+            Console.WriteLine(_timelog);
+            sw.Stop();
             return retVal;
+        }
+
+        private void Log(string section, Stopwatch runningStopwatch)
+        {
+            runningStopwatch.Stop();
+            _timelog += section + ": " + runningStopwatch.ElapsedMilliseconds + "ms" + Environment.NewLine;
+            runningStopwatch.Restart();
         }
     }
 }
